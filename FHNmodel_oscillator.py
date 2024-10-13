e37978fab910171522d4032f02966c0b09421d95
miyazato3 <miyazato.kosuke135@mail.kyutech.jp>
2024-10-13 14:05:47 +0900

    ネットワーク構造と解を保存するように変更した
    後からの解析を可能にするために解とネットワーク構造(隣接行列A)を保存するように変更した。

--- FHNmodel_oscillator.py
+++ FHNmodel_oscillator.py
@@ -12,6 +12,7 @@ from numba import njit
 import time
 import os
 import network
+import pdb
 
 """ 1. 実験パラメータの設定 """
 # 定数の設定
@@ -21,24 +22,30 @@ sigma = 0.1
 a = 0.5
 phi = np.pi/2 - 0.1
 
-# 時間の設定
+# 時間の設定(3時間)
 start_time = 0
-finish_time = 10800    # 3時間
-step_width = 108001
+finish_time = 60
+step_width = 601
+#finish_time = 10800
+#step_width = 108001
 
 # カップリング行列B
 B = np.array([[np.cos(phi), np.sin(phi)], [-np.sin(phi), np.cos(phi)]])
 
 """ 2. ネットワーク構造の定義 """
 """ (現段階ではWSネットワークのみ実装) """
-#network_name = "ws-network"
-#network_name = "unweighted-fractal"
-network_name = "weighted-fractal"
+# 使用するネットワーク構造と構築に必要なパラメータを設定する
+network_name = "ws-network"             # WSネットワーク
+#network_name = "unweighted-fractal"     # 重み無しフラクタル
+#network_name = "weighted-fractal"       # 重み付きフラクタル
+k = 6       # 平均次数 (各ノードが持つ隣接ノードの数)
+p = 0.0     # 再配線確率 (p = 1 で完全なランダムネットワーク)
 
-N = 82  # フラクタル構造ネットワークの場合は82に設定する
-k = 6   # 平均次数 (各ノードが持つ隣接ノードの数)
-p = 0.0   # 再配線確率 (p = 1 で完全なランダムネットワーク)
+# フラクタルネットワークの場合の特別な設定
+if network_name == "unweighted-fractal" or network_name == "weighted-fractal":
+    N = 82
 
+# ネットワーク作成
 A = network.make_network(network_name, N, k, p)
 
 """ 3. 関数の定義 """
@@ -81,6 +88,7 @@ t = np.linspace(start_time, finish_time, step_width)
 # ODE solverをsolve_ivpに変更
 dbg_solve_start_time = time.time()
 sol = solve_ivp(fhn_ode, [start_time, finish_time], X0, method='LSODA', t_eval=t, args=(N, epsilon, sigma, a, A, B))
+pdb.set_trace()
 dbg_solve_end_time = time.time()
 
 # 解(u,v)の取得
@@ -95,8 +103,8 @@ phases = np.array([calculate_phases(u_sol[:, i], v_sol[:, i]) for i in range(len
 r_values = np.array([calculate_r(phases[i, :]) for i in range(len(t))])
 
 
-""" 7. プロット、ログの処理 """
-# 保存するパスを指定
+""" 7. プロット関係、ログの記録"""
+# 画像、パラメータを保存するパスを指定
 root_dir = "results"
 current_time = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 save_path = f"{root_dir}/{current_time}"
@@ -110,6 +118,17 @@ with open(f"{save_path}/parameter.txt", mode="w") as f:
         f.write(f"{param}\t\t: {eval(param)}\n")
 
 # 使用したネットワークの情報を保存(未実装)
+with open(f"{save_path}/network_parameter.txt", mode="w") as f:
+    params = ["network_name", "k", "p"]
+    for param in params:
+        f.write(f"{param}\t\t: {eval(param)}\n")
+
+# ネットワーク構造を保存
+np.savetxt(f"{save_path}/network_structure.txt", A)
+
+# 解を保存
+np.savetxt(f"{save_path}/u_sol.csv", u_sol, delimiter=",")
+np.savetxt(f"{save_path}/v_sol.csv", v_sol, delimiter=",")
 
 # 同期度のプロット
 plt.figure(figsize=(20, 6))
